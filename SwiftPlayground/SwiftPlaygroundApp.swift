29f5b88ad584fe4b38910629fbd5b9c3e0261228
etho <>
2023-03-06 20:51:19 -0500

    push project

---
+++ SwiftPlayground/SwiftPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPlaygroundApp.swift
+//  SwiftPlayground
+//
+//  Created by etho on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
